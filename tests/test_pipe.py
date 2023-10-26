11b229fb42e616d7c79b107c19cc4a53cd9a10a7
Francisco Santos <francismelo.jr@gmail.com>
2023-10-26 15:26:18 -0300

    fix tests

--- tests/test_pipe.py
+++ tests/test_pipe.py
@@ -8,6 +8,27 @@ def test_transform():
     current_directory = os.path.dirname(os.path.realpath(__file__))
     output_dir = os.path.abspath(os.path.join(current_directory, os.pardir, "src", "etl", "output"))
 
-    df = transform(path=output_dir)
+    data1 = {
+    'col1': ['A', 'B', 'C', 'D'],
+    'col2': [10, 20, 30, 40]
+    }
+    df1 = pd.DataFrame(data1)
+
+    data2 = {
+        'Nome': ['E', 'F', 'G', 'H'],
+        'Idade': [50, 60, 70, 80]
+    }
+    df2 = pd.DataFrame(data2)
+
+    path_df1 = os.path.join(output_dir, 'df1.csv')
+    path_df2 = os.path.join(output_dir, 'df2.csv')
+
+    df1.to_csv(path_df1, sep=';')
+    df2.to_csv(path_df2, sep=';')
+
+    os.remove(path_df1)
+    os.remove(path_df2)
+
+    df = transform(output_dir)
 
     assert isinstance(df, pd.DataFrame)
