d720871740133b21d7df79150c2706ba01a34bda
Francisco Santos <francismelo.jr@gmail.com>
2023-10-26 15:51:23 -0300

    added print

--- src/main.py
+++ src/main.py
@@ -8,3 +8,5 @@ if __name__ == '__main__':
     extract('Florianópolis - Santa Catarina')
     df = transform(os.path.dirname(__file__) + '\\etl\\output')
     load(df, 'final')
+
+print('ETL feito!')
